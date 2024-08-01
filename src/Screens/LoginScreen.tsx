aab27cd6ac605a3fc150c0e6cd584d836c50002e
Anant Tiwari <ananttiwariali@gmail.com>
2024-08-01 16:39:37 +0545

    Basic login functionality

---
+++ src/Screens/LoginScreen.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Button, Linking, Text, View} from 'react-native';
+import {
+  generateSpotifyAuthUri,
+  getSpotifyTokensFromCode,
+} from '../helpers/auth';
+
+export default function LoginScreen() {
+  const handleLogin = () => {
+    const spotifyUiUrl = generateSpotifyAuthUri();
+    console.log('Opening the login page: ', spotifyUiUrl);
+    Linking.openURL(spotifyUiUrl);
+  };
+  return (
+    <View>
+      <Text>Login in to Spotify</Text>
+      <Button title="Login" onPress={handleLogin} />
+      <Button title="rough" onPress={async () => {}} />
+    </View>
+  );
+}
