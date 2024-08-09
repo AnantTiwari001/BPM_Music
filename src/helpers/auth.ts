4ce81c3408a00bac26a6ee70da6a776b0c5a9697
Anant Tiwari <ananttiwariali@gmail.com>
2024-08-09 16:53:33 +0545

    updated scope to use only what is needed

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,5 +1,5 @@
 const scope =
-  'streaming user-read-email user-read-private user-read-currently-playing user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state user-library-read playlist-modify-public playlist-modify-private';
+  'playlist-read-private user-library-modify user-top-read playlist-modify-public playlist-modify-private user-read-private user-read-email';
 const spotify_client_id = process.env.SPOTIFY_CLIENT_ID as string;
 const spotify_client_secret = process.env.SPOTIFY_SECRET_KEY as string;
 const spotify_redirect_uri = 'bpm://callback';
